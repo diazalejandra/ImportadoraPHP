7e9e6afc27d0d79fa1e6d28e4370ec43caf3a11e
Alejandra Díaz <diaz.alejandra@gmail.com>
2017-11-21 00:01:24 -0300

    Actualizacion estructura usuario

---
+++ dao/usuarioDAO.php
@@ -0,0 +1,30 @@
+<?php
+
+include_once '../db/conexion.php';
+include_once '../dto/usuarioDTO.php';
+
+class usuarioDAO {
+
+    private $conexion;
+
+    public function __construct($conexion) {
+        $this->conexion = $conexion;
+    }
+
+    public function agregar($registro) {
+        
+    }
+
+    public function eliminar($idRegistro) {
+        
+    }
+
+    public function actualizar($registro) {
+        
+    }
+
+    public function listarTodos() {
+        
+    }
+
+}
