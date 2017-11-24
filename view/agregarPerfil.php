6116d97ade6b9fbf1512c8227a829b8471d0d6b9
Alejandra Díaz <diaz.alejandra@gmail.com>
2017-11-24 00:04:50 -0300

    Se agrega navbar bootstrap a las vistas del mantenedor

--- view/agregarPerfil.php
+++ view/agregarPerfil.php
@@ -1,8 +1,12 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Importadora LTT</title>
+    </head>
+    <body>
+        <?php
+         include_once './view/partial/header.php';
+        ?>
+    </body>
+</html>
