f643d4b2eb2472d137a20afcb1894b5baf794bf2
Alejandra Díaz <diaz.alejandra@gmail.com>
2017-12-01 02:22:11 -0300

    mantenedor perfil

--- view/agregarPerfil.php
+++ view/agregarPerfil.php
@@ -1,3 +1,20 @@
+<?php
+include_once '../controller/Perfil.php';
+include_once '../model/PerfilModel.php';
+
+if (isset($_POST['btn_registro'])) {
+    $perfil = new PerfilModel();
+    $perfil->setId_perfil($_POST['id_perfil']);
+    $perfil->setDescripcion_perfil($_POST['tipo_perfil']);
+
+    if (Perfil::crear($perfil)) {
+        echo "<script type=\"text/javascript\"> alert(\"Perfil creado\");</script>";
+    } else {
+        echo "<script type=\"text/javascript\"> alert(\"Error al crear perfil\");</script>";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -8,9 +25,84 @@
         <?php
         include_once 'partial/header.php';
         ?>
+        <section class="container">
+        <form class="form-horizontal" action="" method="POST">
+            <fieldset>
+
+                <!-- Form Name -->
+                <legend>Tipo de Producto</legend>
+
+                <!-- Text input-->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="id_perfil">Codigo Perfil</label>  
+                    <div class="col-md-4">
+                        <input id="id_perfil" name="id_perfil" id="id_perfil" type="text" placeholder="" class="form-control input-md" required="">
+
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="tipo_perfil">Perfil</label>  
+                    <div class="col-md-4">
+                        <input id="tipo_perfil" name="tipo_perfil" id="tipo_perfil" type="text" placeholder="" class="form-control input-md" required="">
+
+                    </div>
+                </div>
+
+                <!-- Button -->
+                <div class="form-group">
+                    <label class="col-md-4 control-label" for="btn_registro"></label>
+                    <div class="col-md-4">
+                        <button id="btn_registro" name="btn_registro" class="btn btn-primary">Agregar</button>
+                        <button id="btn_listar" type="button" name="btn_listar" class="btn btn-primary">Listar</button>
+                    </div>
+                </div>
+
+            </fieldset>
+        </form>
         
-        <?php
-        include_once 'partial/footer.php';
-        ?>
+        <div class="resultado"></div>
+        </section>
+        
+    <script>
+        $(document).ready(function () {
+
+            $("#btn_listar").on("click", function (event) {
+                agregar();
+            });
+
+            function agregar() {
+                var settings = {
+                    "url": "buscarPerfil.php",
+                    "method": "POST",
+                    "data": {"tipo_perfil": "tipo_perfil"}
+                }
+
+                $.ajax(settings).done(function (response) {
+                    $('.resultado').html('');
+                    $('.resultado').html(response)
+                    eliminar();
+                });
+            }
+
+            function eliminar() {
+                $(".btn_eliminar").on("click", function (e) {
+                    var id = $(this).attr("attr-id");
+                    var settings = {
+                        "url": "buscarPerfil.php",
+                        "method": "POST",
+                        "data": {"id_perfil": id}
+                    }
+
+                    $.ajax(settings).done(function (response) {
+//                        $('.resultadoEli').html('');
+//                        $('.resultadoEli').html(response)
+                        agregar();
+                    });
+                });
+            }
+        });
+
+    </script>
+
     </body>
 </html>
--- view/buscarPerfil.php
+++ view/buscarPerfil.php
@@ -1,12 +1,35 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Importadora LTT</title>
-    </head>
-    <body>
-        <?php
-         include_once './view/partial/header.php';
-        ?>
-    </body>
-</html>
+<?php
+
+include_once '../controller/Perfil.php';
+include_once '../model/PerfilModel.php';
+
+    if (Perfil::listar() != null){
+        $lista = Perfil::listar();
+        echo "<table class='table table-hover table-responsive'>";
+        echo "<tr> ";
+        echo "<th> Codigo </th> ";
+        echo "<th> Descripcion </th> ";
+        echo "<th> Opciones </th> ";
+        echo "</tr>";
+        for ($i = 0; $i < count($lista); $i++) {
+            echo "<tr> ";
+            echo "<td> " . $lista[$i]->getId_perfil() . "</td> ";
+            echo "<td> " . $lista[$i]->getDescripcion_perfil() . "</td> ";
+            echo "<td> <input type=\"button\" class=\"btn_eliminar\" value=\"Eliminar\" attr-id=\"" . $lista[$i]->getId_perfil() . "\" /></td> ";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }else{
+        echo "No existe informacion para mostrar";
+    }
+    
+if(isset($_POST["id_perfil"])){
+if (!Perfil::eliminar($_POST["id_perfil"])) {
+    echo "<script type=\"text/javascript\"> alert(\"No se ha podido eliminar.\");</script>";
+} else {
+
+    echo "<script type=\"text/javascript\"> alert(\"Se ha eliminado el perfil.\");</script>";
+}
+}
+
+
